0559851f58658064ff15dfe13c66939afffd98cd
manos89 <mvahlas@gmail.com>
2022-03-19 23:59:49 +0200

    Add toxarray topandas functions (#36)
    Initial support for exporting PyOphidia cube data to xarray and pandas -based structures

---
+++ PyOphidia/tests/test_extra.py
@@ -0,0 +1,43 @@
+import pytest
+
+from PyOphidia import cube
+from ..extra import *
+
+cube.Cube.setclient()
+try:
+    cube.Cube.createcontainer(container="mytest")
+except RuntimeError:
+    pass
+random_cube_1 = cube.Cube.randcube(container="mytest", dim="lat|lon|k|l|time",
+                                   dim_size="4|2|2|2|1", exp_ndim=4,
+                                   host_partition="main", measure="tos",
+                                   measure_type="double", nfrag=8, ntuple=4,
+                                   nhost=1)
+random_cube_2 = cube.Cube.randcube(container="mytest", dim="lat|lon|time",
+                                   dim_size="4|2|1", exp_ndim=2,
+                                   host_partition="main", measure="tos",
+                                   measure_type="double", nfrag=4, ntuple=2,
+                                   nhost=1)
+random_cube_3 = cube.Cube(
+    src_path='/public/data/ecas_training/tasmax_day_CMCC'
+             '-CESM_rcp85_r1i1p1_20960101-21001231.nc',
+    measure='tasmax',
+    import_metadata='yes',
+    imp_dim='time',
+    imp_concept_level='d', vocabulary='CF',
+    hierarchy='oph_base|oph_base|oph_time',
+    ncores=4,
+    description='Max Temps'
+)
+
+
+@pytest.mark.parametrize("cube",
+                         [random_cube_1, random_cube_2, random_cube_3])
+def test_convert_to_xarray(cube):
+    ds = convert_to_xarray(cube)
+
+
+@pytest.mark.parametrize(("cube"),
+                         [(random_cube_1), (random_cube_2), (random_cube_3)])
+def test_convert_to_dataframe(cube):
+    df = convert_to_dataframe(cube)
