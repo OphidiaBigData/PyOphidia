549624bee1c9229e1e84eafc5e9d6eb0d0dd1945
PalomoIFCA <palomo@ifca.unican.es>
2024-07-03 13:41:55 +0200

    Changed wisvalid json parsing

--- PyOphidia/client.py
+++ PyOphidia/client.py
@@ -901,16 +901,16 @@ class Client:
             return False
         w = None
 
-        # Remove comment blocks
-        checked_workflow = re.sub(re.compile(r"/\*.*?\*/|//.*?\n", re.DOTALL), "\n", workflow)
-
-        if isinstance(checked_workflow, str):
+        if isinstance(workflow, str):
             try:
+                # Remove comment blocks in strings
+                checked_workflow = re.sub(r"(?m)^ *#.*\n?", "", workflow)
                 w = json.loads(checked_workflow)
             except ValueError:
                 return False, "Workflow is not a valid JSON"
-        elif isinstance(checked_workflow, dict):
-            w = checked_workflow
+                
+        elif isinstance(workflow, dict):
+            w = workflow
         else:
             return False, "Workflow is not a valid dictionary"
         if "name" not in w or not w["name"]:
