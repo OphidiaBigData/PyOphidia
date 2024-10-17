3d8fb0105838d9dd28dfc5399c8bd185f83f6a19
Cosimo Palazzo <cosimo.palazzo@cmcc.it>
2024-10-17 20:39:00 +0200

    Amend to previous commit

--- examples/coldspells.sh
+++ examples/coldspells.sh
@@ -7,4 +7,7 @@ curl -s -O -k 'https://esgf-node.cmcc.it/thredds/fileServer/esg_dataroot/cmip5/o
 
 cd /usr/local/ophidia/model
 curl -s -O -k 'https://esgf-node.cmcc.it/thredds/fileServer/esg_dataroot/cmip5/output1/CMCC/CMCC-CESM/rcp85/day/atmos/day/r1i1p1/v20170725/tasmin/tasmin_day_CMCC-CESM_rcp85_r1i1p1_20960101-21001231.nc'
+cdo -splityear -delete,dom=29feb tasmin_day_CMCC-CESM_rcp85_r1i1p1_20960101-21001231.nc year
+
+exit 0
 
