b0c159ba9ae3888692a2f7b82030675e86e67aa1
Cosimo Palazzo <cosimo.palazzo@cmcc.it>
2024-11-06 19:48:02 +0100

    Update ColdSpells notebook

--- examples/coldspells.sh
+++ examples/coldspells.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 mkdir -p /usr/local/ophidia/{historical,model}
 
 cd /usr/local/ophidia/historical
@@ -7,7 +9,6 @@ curl -s -O -k 'https://esgf-node.cmcc.it/thredds/fileServer/esg_dataroot/cmip5/o
 
 cd /usr/local/ophidia/model
 curl -s -O -k 'https://esgf-node.cmcc.it/thredds/fileServer/esg_dataroot/cmip5/output1/CMCC/CMCC-CESM/rcp85/day/atmos/day/r1i1p1/v20170725/tasmin/tasmin_day_CMCC-CESM_rcp85_r1i1p1_20960101-21001231.nc'
-cdo -splityear -delete,dom=29feb tasmin_day_CMCC-CESM_rcp85_r1i1p1_20960101-21001231.nc year
 
 exit 0
 
